4a162acb159e0de8b29b2ca49e177b4a1c5ef1a9
JakolA <leh-1414@yandex.ru>
2025-06-08 18:22:05 +0300

    Добавлен барак на карту

--- main.py
+++ main.py
@@ -8,35 +8,63 @@ from PyQt5.QtWidgets import (
     QGraphicsPixmapItem
 )
 from PyQt5.QtGui import QPixmap
+from typing import List
 
 
-def run():
-    app = QApplication([])
+class Game:
+    def __init__(self, name):
+        self.name = name
 
-    window = QWidget()
-    window.setWindowTitle("Game")
+        app = QApplication([])
+        wnd = self.init_window()
+        wnd.show()
+        app.exec_()
 
-    scene = QGraphicsScene()
+    def init_window(self):
+        window = QWidget()
+        window.setWindowTitle(self.name)
 
-    pixmap = QPixmap('media/map.png')
-    map_item = QGraphicsPixmapItem(pixmap)
-    map_item.setPos(0, 0)
-    scene.addItem(map_item)
+        window.resize(1430, 900)
 
-    exit_button = QPushButton("Exit")
-    exit_button.clicked.connect(window.close)
+        exit_button = QPushButton("Exit")
+        exit_button.setFixedSize(30, 30)
+        exit_button.clicked.connect(window.close)
 
-    view = QGraphicsView(scene)
+        view = self.init_view()
+        layout = self.layouting(view=view, objects=[exit_button])
+        window.setLayout(layout)
 
-    layout = QVBoxLayout()
-    layout.addWidget(view)
-    layout.addWidget(exit_button)
-    window.setLayout(layout)
+        return window
 
-    window.show()
+    @staticmethod
+    def init_view():
+        scene = QGraphicsScene()
 
-    app.exec_()
+        pixmap = QPixmap('media/map.png')
+        map_item = QGraphicsPixmapItem(pixmap)
+        map_item.setPos(0, 0)
+
+        barrack = QPixmap('media/barracks.png')
+        barrack_item = QGraphicsPixmapItem(barrack)
+        barrack_item.setPos(0, 119)
+
+        scene.addItem(map_item)
+        scene.addItem(barrack_item)
+
+        view = QGraphicsView(scene)
+        return view
+
+    @staticmethod
+    def layouting(view, objects: List):
+        layout = QVBoxLayout()
+
+        layout.addWidget(view)
+
+        for obj in objects:
+            layout.addWidget(obj)
+
+        return layout
 
 
 if __name__ == '__main__':
-    run()
+    Game('Game')
