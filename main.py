e608c3a8eb338832c6f599bdcba1b0ee3dc61d4e
JakolA <leh-1414@yandex.ru>
2025-06-08 15:11:02 +0300

    Added simple map with exit button. Used PyQt5

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
-# Tower offense
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QVBoxLayout,
+    QPushButton,
+    QGraphicsScene,
+    QGraphicsView,
+    QGraphicsPixmapItem
+)
+from PyQt5.QtGui import QPixmap
 
 
 def run():
-    pass
+    app = QApplication([])
+
+    window = QWidget()
+    window.setWindowTitle("Game")
+
+    scene = QGraphicsScene()
+
+    pixmap = QPixmap('media/map.png')
+    map_item = QGraphicsPixmapItem(pixmap)
+    map_item.setPos(0, 0)
+    scene.addItem(map_item)
+
+    exit_button = QPushButton("Exit")
+    exit_button.clicked.connect(window.close)
+
+    view = QGraphicsView(scene)
+
+    layout = QVBoxLayout()
+    layout.addWidget(view)
+    layout.addWidget(exit_button)
+    window.setLayout(layout)
+
+    window.show()
+
+    app.exec_()
 
 
 if __name__ == '__main__':
